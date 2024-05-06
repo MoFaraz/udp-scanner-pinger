99874a0bcf7f9cbfc0c90d964eaf8d6f1952355e
MoFaraz <mopharaz@gmail.com>
2024-05-06 12:56:43 +0330

    define a function for pinger_server

--- udp_pinger_server.py
+++ udp_pinger_server.py
@@ -1,21 +1,17 @@
 import random
-from socket import *
+import socket
 
-# Create a UDP socket
-# Notice the use of SOCK_DGRAM for UDP packets
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-# Assign IP address and port number to socket
-serverSocket.bind(('', 12000))
-print("Started UDP server on port 12000")
-while True:
-    try:
+def start_server(target, port):
+    host = socket.gethostbyname(target)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind((host, port))
+    print("Started UDP server on port 12000")
+    while True:
         rand = random.randint(0, 10)
-        message, address = serverSocket.recvfrom(1024)
+        message, address = udp_socket.recvfrom(1024)
         message = message.upper()
-        print('Message {} is recived'.format(message))
+        print(f'Message {message} is recived')
         if rand < 4:
-         continue
-        serverSocket.sendto(message, address)
-    except KeyboardInterrupt:
-       print("good bye")
-       break
+            continue
+        udp_socket.sendto(message, address)
+        
