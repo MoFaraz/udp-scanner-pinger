f199563d988de5b4907edf676654f9f4d3da6f6c
MoFaraz <mopharaz@gmail.com>
2024-05-06 12:56:20 +0330

    define a function for pinger_client

--- udp_pinger_client.py
+++ udp_pinger_client.py
@@ -1,27 +1,31 @@
 import socket
 import time
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_addr = ('127.0.0.1', 12000)
-sock.settimeout(1)
+def find_rtt(target, port):
+    host = socket.gethostbyname(target)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_addr = (host, port)
+    udp_socket.settimeout(1)
 
-try:
-    for i in range(1, 11):
-        start = time.time()
-        message = 'Ping #' + str(i) + " " + time.ctime(start)
-        message1 = message.encode()
-        try:
-            sent = sock.sendto(message1, server_addr)
-            print("Sent " + message)
-            data, server = sock.recvfrom(4096)
-            data = data.decode()
-            print("Received " + data)
-            end = time.time();
-            elapsed = end - start
-            print("RTT: " + str(elapsed) + " seconds\n")
-        except socket.timeout:
-            print("#" + str(i) + " Requested Time out\n")
+    try:
+        for i in range(1, 11):
+            start = time.time()
+            message = 'Ping #' + str(i) + " " + time.ctime(start)
+            message1 = message.encode()
+            try:
+                sent = udp_socket.sendto(message1, server_addr)
+                print("Sent " + message)
+                data, server = udp_socket.recvfrom(4096)
+                data = data.decode()
+                print("Received " + data)
+                end = time.time();
+                elapsed = end - start
+                print("RTT: " + str(elapsed) + " seconds\n")
+            except socket.timeout:
+                print("#" + str(i) + " Requested Time out\n")
+            except socket.error:
+                print("something went wrong")
 
-finally:
-    print("closing socket")
-    sock.close()
+    finally:
+        print("closing socket")
+        udp_socket.close()
