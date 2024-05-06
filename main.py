1048f03765688712b17900d3ddf98f6c1aeab9c5
MoFaraz <mopharaz@gmail.com>
2024-05-06 12:57:57 +0330

    add new functions to the main file

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from udp_scanner import Udp_Socket
-import subprocess
+from udp_scanner import Udp_Socket, Tcp_Socket
+from udp_pinger_client import find_rtt
+from udp_pinger_server import start_server
+import threading
+
 
 if __name__ == "__main__":
     try: 
         while True:
             user_input = input("\n[1] udp-port-scanner  [2] udp-pinger\n")
-        
             if user_input == '1':
                 Udp_Socket('localhost')
+                #Tcp_Socket('localhost')
             elif user_input == '2':
-                exec(open('udp_pinger_client.py').read())                
-            elif user_input.lower() == 'q':
-                print("Exiting program.")
-                break
+                server_thread = threading.Thread(target=start_server, args=('localhost',12000))
+                server_thread.start()
+                find_rtt('localhost', 12000)
+            else:
+                print("Please Input Numer [1] or [2]")
     except KeyboardInterrupt:
         print('good bye')
     
