10f67e87dc42ef0d1cb9486eb300810b64df6dd4
MoFaraz <mopharaz@gmail.com>
2024-05-10 11:07:37 +0330

    server_thread modified to closed after program ended

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from udp_scanner import Udp_Socket, Tcp_Socket
 from udp_pinger_client import find_rtt
 from udp_pinger_server import start_server
 import threading
-
+import sys
 
 if __name__ == "__main__":
+    server_thread = threading.Thread(target=start_server, args=('localhost',12000))
+    server_thread.daemon = True
+    server_thread.start()
     try: 
         while True:
             user_input = input("\n[1] udp-port-scanner  [2] udp-pinger\n")
@@ -12,12 +15,10 @@ if __name__ == "__main__":
                 Udp_Socket('localhost')
                 #Tcp_Socket('localhost')
             elif user_input == '2':
-                server_thread = threading.Thread(target=start_server, args=('localhost',12000))
-                server_thread.start()
                 find_rtt('localhost', 12000)
             else:
                 print("Please Input Numer [1] or [2]")
     except KeyboardInterrupt:
         print('good bye')
-    
+        sys.exit()
    
